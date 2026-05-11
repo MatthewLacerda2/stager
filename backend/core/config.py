cfba823f3b6cc2a81b7a9ebd663d924df9c6e561
unknown <math2.0@hotmail.com>
2026-05-11 00:58:51 -0300

    fix lack database_url

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,12 +1,25 @@
 from functools import lru_cache
+from pydantic import model_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     PROJECT_NAME: str = "Stager"
     
-    DATABASE_URL: str
+    POSTGRES_SERVER: str = "db"
+    POSTGRES_PORT: str = "5432"
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = ""
+    POSTGRES_DB: str = "stager"
+    
+    DATABASE_URL: str | None = None
     TEST_DATABASE_URL: str | None = None  # Optional, only needed for tests
+    
+    @model_validator(mode="after")
+    def assemble_db_url(self) -> "Settings":
+        if not self.DATABASE_URL:
+            self.DATABASE_URL = f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        return self
     
     model_config = SettingsConfigDict(
         case_sensitive=True, 
